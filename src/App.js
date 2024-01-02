d616a6d1e67c14512cdf1fcda1a8734bd8c04485
rafael <okenyirafael@gmail.com>
2024-01-02 09:34:31 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import SignIn from './SignIn'
 import {Routes, Route} from 'react-router-dom'
 import Welcome from './Welcome'
 import DetectES from './DetectES'
@@ -7,8 +6,7 @@ import DetectES from './DetectES'
 const App = () => {
   return (
    <Routes>
-    <Route path='/' exact element={<SignIn/>} />
-    <Route path='welcome' element={<Welcome/>} />
+    <Route path='/' exact element={<Welcome/>} />
     <Route path='tests' element={<DetectES/>} />
    </Routes>
   )
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import eye from'./assets/eye.png'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Welcome = () => {
-  const location = useLocation()
   const navigate = useNavigate()
-  const user = location.state?.data
   
   function handleClick (){
      navigate('/tests')
@@ -21,7 +19,7 @@ const Welcome = () => {
 
           <section>
             
-            <h2 className='welcome' >Welcome <span>{user}</span> , to IrisAI</h2>
+            <h2 className='welcome' >Welcome to IrisAI</h2>
             <p className='p' >Iris AI is an expert system that helps you diagnose some select eye defects you may have.</p>
             
           </section>
